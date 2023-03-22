e9ece57d1fed83fd870dceeb5699691f5b7792f0
Eddie Naff <eddienaff@gmail.com>
2023-03-21 20:36:57 -0700

    Add progress bar handling function

--- src/main.ts
+++ src/main.ts
@@ -20,10 +20,19 @@ document.addEventListener("click", function (this, evt) {
   }
 });
 
+window.addEventListener("resize", function (this, evt) {});
+
+document.querySelectorAll(".progress-bar").forEach((item) => {
+  calculateProgressBar(item);
+});
+
 function onHandleClick(handle: Element) {
+  const progressBar = handle
+    .closest(".movie-grouping")
+    ?.querySelector(".progress-bar");
   const container = handle.closest(".container");
   // Confirm that container is not null
-  if (container) {
+  if (container && progressBar) {
     const slider = container.querySelector<HTMLElement>(".slider");
     console.log("VIEW SLIDER", slider);
 
@@ -38,6 +47,9 @@ function onHandleClick(handle: Element) {
           "--slider-index",
           (sliderIndex - 1).toString()
         );
+        // Change the active progress bar item
+        progressBar.children[sliderIndex].classList.remove("active");
+        progressBar.children[sliderIndex - 1].classList.add("active");
       }
     }
     // Right handler button
@@ -56,7 +68,43 @@ function onHandleClick(handle: Element) {
           "--slider-index",
           (sliderIndex + 1).toString()
         );
+
+        // Change the active progess bar item
+        progressBar.children[sliderIndex].classList.remove("active");
+        progressBar.children[sliderIndex + 1].classList.add("active");
       }
     }
+  }
+}
+
+function calculateProgressBar(progressBar: Element) {
+  progressBar.innerHTML = "";
+
+  const grouping = progressBar.closest(".movie-grouping");
+
+  if (!grouping) {
+    return;
+  }
+  const slider = grouping.querySelector<HTMLDivElement>(".slider");
+  if (!slider) {
+    return;
+  }
+  const sliderIndex = parseInt(
+    getComputedStyle(slider).getPropertyValue("--slider-index")
+  );
+  const itemCount = slider.children.length;
+  const itemsPerScreen = parseInt(
+    getComputedStyle(slider).getPropertyValue("--items-per-screen")
+  );
+  const progressBarItemsCount = Math.ceil(itemCount / itemsPerScreen);
+
+  // Add a progress tab for each item
+  for (let index = 0; index < progressBarItemsCount; index++) {
+    const barItem = document.createElement("div");
+    barItem.classList.add("progress-item");
+    if (index === sliderIndex) {
+      barItem.classList.add("active");
+    }
+    progressBar.append(barItem);
   }
 }
